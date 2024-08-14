9c3708188524c5e21610cc9db44780ba658cc758
AngelloFD <fallaangello@gmail.com>
2024-08-14 16:29:01 -0500

    Procedure de refresh completo y correcto

--- src/procedures/refreshRating.js
+++ src/procedures/refreshRating.js
@@ -1,49 +1,52 @@
-const { CronJob } = require('cron');
+import { CronJob } from 'cron';
 const { QueryTypes } = require('sequelize');
 const sequelizeInstance = require('../utils/database.js');
 
+// Cron job that runs the refreshRating procedure every 1 minute
 // Asyncronous procedure that runs monthly to calculate and refresh the rating of all the professors
-const refreshRating = async () => {
-  try {
-    // Query to get all the professors
-    const professors = await sequelizeInstance.query(
-      'SELECT * FROM professors',
-      {
-        type: QueryTypes.SELECT,
-      }
-    );
-
-    // Loop through all the professors
-    for (const professor of professors) {
-      // Query to get all the ratings of the professor
-      const ratings = await sequelizeInstance.query(
-        `SELECT rating FROM comments WHERE professorId = ${professor.id}`,
+const refreshRatingsJob = new CronJob(
+  '0 0 1 * *',
+  async function () {
+    try {
+      console.info('Running the refreshRating procedure...');
+      // Query to get all the professors
+      const professors = await sequelizeInstance.query(
+        'SELECT * FROM professors',
         {
           type: QueryTypes.SELECT,
         }
       );
 
-      // Calculate the rating of the professor
-      let rating = 0;
-      for (const ratingObj of ratings) {
-        rating += ratingObj.rating;
+      // Loop through all the professors
+      for (const professor of professors) {
+        // Query to get all the ratings of the professor
+        const ratings = await sequelizeInstance.query(
+          `SELECT rating FROM comments WHERE professorId = ${professor.id}`,
+          {
+            type: QueryTypes.SELECT,
+          }
+        );
+
+        // Calculate the rating of the professor
+        let rating = 0;
+        for (const ratingObj of ratings) {
+          rating += ratingObj.rating;
+        }
+        rating /= ratings.length;
+
+        // Update the rating of the professor in the database
+        await sequelizeInstance.query(
+          `UPDATE professors SET averageRating = ${rating} WHERE id = ${professor.id}`
+        );
       }
-      rating /= ratings.length;
-
-      // Update the rating of the professor in the database
-      await sequelizeInstance.query(
-        `UPDATE professors SET averageRating = ${rating} WHERE id = ${professor.id}`
-      );
+      console.info('The refreshRating procedure has finished successfully!');
+    } catch (error) {
+      console.error(`Error during the refreshRating procedure: ${error}`);
     }
-  } catch (error) {
-    console.error(`Error during the refreshRating procedure: ${error}`);
-  }
-};
+  },
+  null,
+  true,
+  'America/Lima'
+);
 
-// Cron job that runs the refreshRating procedure every 1 minute
-const refreshRatingsJob = new CronJob('0 0 1 * *', refreshRating, {
-  start: true,
-  timeZone: 'America/Lima',
-});
-
-module.exports = refreshRatingsJob;
+export default refreshRatingsJob;
