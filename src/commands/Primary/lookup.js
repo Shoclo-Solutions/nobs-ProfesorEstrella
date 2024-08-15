531e75847221c8603f7f8dfa1f2645c71ca42c7a
AngelloFD <fallaangello@gmail.com>
2024-08-15 00:27:27 -0500

    Comando estable

--- src/commands/Primary/lookup.js
+++ src/commands/Primary/lookup.js
@@ -1,7 +1,15 @@
 const { Op } = require('sequelize');
-const { EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js');
+const {
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require('discord.js');
 const Professor = require('../../models/profesor');
 const Courses = require('../../models/curso');
+const Comments = require('../../models/comentario');
 
 module.exports = {
   /** @type {import('commandkit').CommandData}  */
@@ -21,99 +29,24 @@ module.exports = {
   /**
    * @param {import('commandkit').SlashCommandProps} param0
    */
-  run: async ({ interaction }) => {
-    /** @type {String} */
+  run: async ({ interaction, client }) => {
     const paramProfe = interaction.options.getString('profesor');
     const pageSize = 4;
     let page = 1;
 
-    /**
-     * @param {number} page
-     * @returns {Promise<{ professors: Professor[], count: number }>}
-     */
-    const fetchProfessors = async (page) => {
-      const offset = (page - 1) * pageSize;
-      const { rows: professors, count } = await Professor.findAndCountAll({
-        where: {
-          fullname: {
-            [Op.like]: `%${paramProfe}%`,
-          },
-        },
-        limit: pageSize,
-        offset: offset,
-      });
-      return { professors, count };
-    };
-
-    /**
-     * @param {Professor[]} professors
-     * @param {number} page
-     * @param {number} totalPages
-     * @returns {{ embed: import('discord.js').MessageEmbed, row: import('discord.js').MessageActionRow }}
-     */
-    const createEmbed = (professors, page, totalPages) => {
-      const embed = new EmbedBuilder()
-        .setTitle(`Resultados para ${paramProfe}`)
-        .setDescription(
-          professors.map((prof) => prof.fullname).join('\n') ||
-            'No se encontraron resultados.'
-        )
-        .setColor('Blue')
-        .setTimestamp()
-        .setFooter({ text: `Página ${page} de ${totalPages}` });
-
-      const buttons = professors.map((prof, index) =>
-        new ButtonBuilder()
-          .setCustomId(`prof_${prof.id}`)
-          .setLabel(prof.fullname)
-          .setStyle(1)
-          .setEmoji('🧑‍🏫')
-      );
-
-      const row = new ActionRowBuilder().addComponents(buttons);
-
-      return { embed, row };
-    };
-
-    /**
-     * @param {Professor} professor
-     * @returns {import('discord.js').MessageEmbed}
-     */
-    const createDetailEmbed = async (professor) => {
-      const courses = await Courses.findAll({
-        where: {
-          professorId: professor.id,
-        },
-      });
-
-      const coursesNames =
-        courses.map((course) => course.name).join(', ') || 'N/A';
-
-      const embed = new EmbedBuilder()
-        .setTitle(professor.fullname)
-        .setDescription(`Detalles del profesor ${professor.fullname}`)
-        .addFields(
-          { name: 'Nombre Completo', value: professor.fullname || 'N/A' },
-          { name: 'Contrato', value: professor.contract || 'N/A' },
-          { name: 'Correo institucional', value: professor.email || 'N/A' },
-          {
-            name: 'Cursos',
-            value: coursesNames,
-          },
-          {
-            name: 'Calificación Promedio',
-            value: professor.averageRating?.toString() || 'N/A',
-          }
-        )
-        .setColor('Green')
-        .setTimestamp();
-      return embed;
-    };
-
-    const { professors, count } = await fetchProfessors(page);
+    const { professors, count } = await fetchProfessors(
+      paramProfe,
+      page,
+      pageSize
+    );
     const totalPages = Math.ceil(count / pageSize);
 
-    const { embed, row } = createEmbed(professors, page, totalPages);
+    const { embed, row } = createEmbed(
+      professors,
+      paramProfe,
+      page,
+      totalPages
+    );
 
     const embedMessage = await interaction.reply({
       embeds: [embed],
@@ -122,50 +55,17 @@ module.exports = {
     });
 
     if (totalPages > 1) {
-      await embedMessage.react('⬅️');
-      await embedMessage.react('➡️');
-
-      const filter = (reaction, user) => {
-        return ['⬅️', '➡️'].includes(reaction.emoji.name) && !user.bot;
-      };
-
-      const collector = embedMessage.createReactionCollector({
-        filter,
-        time: 60000,
-      });
-
-      collector.on('collect', async (reaction, user) => {
-        if (reaction.emoji.name === '⬅️' && page > 1) {
-          page--;
-        } else if (reaction.emoji.name === '➡️' && page < totalPages) {
-          page++;
-        }
-
-        const { professors } = await fetchProfessors(page);
-        const { embed, row } = createEmbed(professors, page, totalPages);
-        await embedMessage.edit({
-          embeds: [embed],
-          components: [row],
-        });
-        await reaction.users.remove(user.id);
-      });
+      await addPaginationReactions(embedMessage);
+      setupPaginationCollector(
+        embedMessage,
+        paramProfe,
+        page,
+        totalPages,
+        pageSize
+      );
     }
 
-    const buttonFilter = (i) =>
-      i.customId.startsWith('prof_') && i.user.id === interaction.user.id;
-    const buttonCollector = embedMessage.createMessageComponentCollector({
-      filter: buttonFilter,
-      time: 60000,
-    });
-
-    buttonCollector.on('collect', async (i) => {
-      const professorId = i.customId.split('_')[1];
-      const selectedProfessor = await Professor.findByPk(professorId);
-      if (selectedProfessor) {
-        const detailEmbed = await createDetailEmbed(selectedProfessor);
-        await i.update({ embeds: [detailEmbed], components: [] });
-      }
-    });
+    setupButtonCollector(embedMessage, interaction);
   },
 
   /** @type {import('commandkit').CommandOptions} */
@@ -177,4 +77,173 @@ module.exports = {
       'ManageMessages',
     ],
   },
+};
+
+/**
+ * Fetches professors from the database according to the search term and page number.
+ * @param {string} paramProfe
+ * @param {number} page
+ * @param {number} pageSize
+ * @returns {Promise<{ professors: Professor[], count: number }>}
+ */
+const fetchProfessors = async (paramProfe, page, pageSize) => {
+  const offset = (page - 1) * pageSize;
+  const { rows: professors, count } = await Professor.findAndCountAll({
+    where: {
+      fullname: {
+        [Op.like]: `%${paramProfe}%`,
+      },
+    },
+    limit: pageSize,
+    offset: offset,
+  });
+  return { professors, count };
+};
+
+/**
+ * Creates an embed with the list of professors.
+ * @param {Professor[]} professors
+ * @param {string} paramProfe
+ * @param {number} page
+ * @param {number} totalPages
+ * @returns {{ embed: import('discord.js').EmbedBuilder, row: import('discord.js').ActionRowBuilder }}
+ */
+const createEmbed = (professors, paramProfe, page, totalPages) => {
+  const embed = new EmbedBuilder()
+    .setTitle(`Resultados para ${paramProfe}`)
+    .setDescription(
+      professors.map((prof) => prof.fullname).join('\n') ||
+        'No se encontraron resultados.'
+    )
+    .setColor('Blue')
+    .setTimestamp()
+    .setFooter({ text: `Página ${page} de ${totalPages}` });
+
+  const buttons = professors.map((prof) =>
+    new ButtonBuilder()
+      .setCustomId(`prof_${prof.id}`)
+      .setLabel(prof.fullname)
+      .setStyle(1)
+      .setEmoji('🧑‍🏫')
+  );
+
+  const row = new ActionRowBuilder().addComponents(buttons);
+
+  return { embed, row };
+};
+
+/**
+ * Adds pagination reactions to the message.
+ * @param {import('discord.js').Message} embedMessage
+ */
+const addPaginationReactions = async (embedMessage) => {
+  await embedMessage.react('⬅️');
+  await embedMessage.react('➡️');
+};
+
+/**
+ * Sets up the pagination collector.
+ * @param {import('discord.js').Message} embedMessage
+ * @param {string} paramProfe
+ * @param {number} page
+ * @param {number} totalPages
+ * @param {number} pageSize
+ */
+const setupPaginationCollector = (
+  embedMessage,
+  paramProfe,
+  page,
+  totalPages,
+  pageSize
+) => {
+  const filter = (reaction, user) => {
+    return ['⬅️', '➡️'].includes(reaction.emoji.name) && !user.bot;
+  };
+
+  const collector = embedMessage.createReactionCollector({
+    filter,
+    time: 60000,
+  });
+
+  collector.on('collect', async (reaction, user) => {
+    if (reaction.emoji.name === '⬅️' && page > 1) {
+      page--;
+    } else if (reaction.emoji.name === '➡️' && page < totalPages) {
+      page++;
+    }
+
+    const { professors } = await fetchProfessors(paramProfe, page, pageSize);
+    const { embed, row } = createEmbed(
+      professors,
+      paramProfe,
+      page,
+      totalPages
+    );
+    await embedMessage.edit({
+      embeds: [embed],
+      components: [row],
+    });
+    await reaction.users.remove(user.id);
+  });
+};
+
+/**
+ * Sets up the button collector.
+ * @param {import('discord.js').Message} embedMessage
+ * @param {import('commandkit').SlashCommandProps} commandInteraction
+ */
+const setupButtonCollector = (embedMessage, commandInteraction) => {
+  const buttonFilter = (i) =>
+    i.customId.startsWith('prof_') && i.user.id === commandInteraction.user.id;
+  const buttonCollector = embedMessage.createMessageComponentCollector({
+    filter: buttonFilter,
+    time: 60000,
+  });
+
+  buttonCollector.on('collect', async (i) => {
+    const professorId = i.customId.split('_')[1];
+    const selectedProfessor = await Professor.findByPk(professorId);
+    if (selectedProfessor) {
+      const detailEmbed = await createDetailEmbed(selectedProfessor);
+      await i.update({ embeds: [detailEmbed], components: [] });
+    }
+  });
+};
+
+/**
+ * Creates a detailed embed for a professor.
+ * @param {Professor} professor
+ * @returns {Promise<import('discord.js').EmbedBuilder>}
+ */
+const createDetailEmbed = async (professor) => {
+  const courses = await Courses.findAll({
+    where: {
+      professorId: professor.id,
+    },
+  });
+
+  const coursesNames = courses.map((course) => course.name).join(', ') || 'N/A';
+
+  const embed = new EmbedBuilder()
+    .setTitle(professor.fullname)
+    .setDescription(`Detalles del profesor ${professor.fullname}`)
+    .addFields(
+      { name: 'Nombre Completo', value: professor.fullname || 'N/A' },
+      { name: 'Contrato', value: professor.contract || 'N/A' },
+      { name: 'Correo institucional', value: professor.email || 'N/A' },
+      {
+        name: 'Cursos',
+        value: coursesNames,
+      },
+      {
+        name: 'Calificación Promedio',
+        value: professor.averageRating?.toString() || 'N/A',
+      }
+    )
+    .setColor('Green')
+    .setTimestamp()
+    .setFooter({
+      text: 'Reacciona con ✏️ para añadir un comentario o 🔙 para volver a la lista',
+    });
+  return embed;
 };
