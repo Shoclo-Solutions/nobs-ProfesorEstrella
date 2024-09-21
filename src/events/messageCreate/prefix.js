d2438e0675e21908fb1077f8527839ef50122060
AngelloFD <fallaangello@gmail.com>
2024-09-21 16:02:52 -0500

    Eliminación de comentarios de usuarios baneados

--- src/events/messageCreate/prefix.js
+++ src/events/messageCreate/prefix.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const BannedTable = require('../../models/banned');
+const Comment = require('../../models/comentario');
 
 /** @param {import('discord.js').Message} message */
 module.exports = async (message) => {
@@ -10,6 +11,7 @@ module.exports = async (message) => {
     const args = message.content.slice(1).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
+    // Prefix command logic
     if (command === 'ban') {
         const user = message.mentions.users.first();
         if (!user) return message.reply('You need to mention a user to ban them!');
@@ -27,6 +29,35 @@ module.exports = async (message) => {
 
         if (!created) return message.reply('That user is already banned!');
 
-        message.reply(`Successfully banned ${user.tag}`);
+        const user_has_comments = await Comment.findOne({
+            where: {
+                by: user.id,
+            }
+        });
+
+        if (!user_has_comments) return message.reply('That user has no comments to delete. They have been banned.');
+
+        await Comment.update({
+            deletedAt: Date.now(),
+        }, {
+            where: {
+                by: user.id,
+            }
+        })
+
+        return message.reply(`Successfully banned ${user.tag}`);
+    } else if (command === 'unban') {
+        const user = message.mentions.users.first();
+        if (!user) return message.reply('You need to mention a user to unban them!');
+        
+        const deleted = await BannedTable.destroy({
+            where: {
+                discordId: user.id,
+            }
+        });
+
+        if (deleted === 0) return message.reply('That user is not banned!');
+
+        return message.reply(`Successfully unbanned ${user.tag}`);
     }
 };
